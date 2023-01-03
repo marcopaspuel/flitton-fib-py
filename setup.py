2d608c24ba64a0e1750600a6523d520867b436fa
Marco.Paspuel <marco.paspuel@helsing.ai>
2023-01-03 09:17:22 +0100

    feat: add readme ad long description

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from setuptools import find_packages, setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="flitton_fib_py",
     version="0.0.1",
     author="Marco Paspuel",
     author_email="marco.paspuel@outlook.com",
     description="Calculates a Fibonacci number",
-    long_description="A basic library that calculates Fibonacci numbers",
+    long_description=long_description,
     long_description_context_type="text/markdown",
     url="https://github.com/marcopaspuel/flitton-fib-py",
     install_requires=[],
