45ee74e6933bcb9059c890465f6612a37380d2f3
Marco.Paspuel <marco.paspuel@helsing.ai>
2023-01-03 09:10:41 +0100

    feat: add setup to module

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="flitton_fib_py",
+    version="0.0.1",
+    author="Marco Paspuel",
+    author_email="marco.paspuel@outlook.com",
+    description="Calculates a Fibonacci number",
+    long_description="A basic library that calculates Fibonacci numbers",
+    long_description_context_type="text/markdown",
+    url="https://github.com/marcopaspuel/flitton-fib-py",
+    install_requires=[],
+    packages=find_packages(exclude="tests,"),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programing Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3',
+    test_require=['pytest'],
+)
